9abd0f2ff9dbcfd1849d391f7a3463f34d972009
Emerson Lima <emerson.dev7@gmail.com>
2022-07-31 19:54:47 -0300

    finished app

--- src/screens/SingIn.tsx
+++ src/screens/SingIn.tsx
@@ -1,9 +1,22 @@
-import {View, Text} from 'react-native'
+import {VStack, Heading, Icon} from 'native-base'
+import { Envelope, Key} from 'phosphor-react-native'
+
+import Logo from '../assets/logo_primary.svg';
+import { Input } from '../components/Input';
 
 export function SignIn(){
   return(
-    <View>
-    <Text>Olá, Emerson</Text>
-  </View>
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo/>
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+      <Input 
+      placeholder="E-mail"
+      mb={5}
+      InputLeftElement={<Icon as={< Envelope/>} />}
+      />
+      <Input placeholder="Senha" />
+  </VStack>
   )
 }
